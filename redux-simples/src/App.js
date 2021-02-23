5b699852cab9a8fa3da93edad9e965bcec5caa7f
DigitalHDR <digitalextremeultra@gmail.com>
2021-02-23 13:58:09 -0300

    v1

--- redux-simples/src/App.js
+++ redux-simples/src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import Card from './components/Card'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Exercício React-Redux (Simples)</h1>
+      <div className="linha">
+        <Card title="Card 1" Red>X</Card>
+      </div>
+      <div className="linha">
+        <Card title="Card 2" Green>X</Card>
+        <Card title="Card 3" Blue>Y</Card>
+        <Card title="Card 4" Purple>Y</Card>
+      </div>
     </div>
-  );
+  )
 }
 
 export default App;
